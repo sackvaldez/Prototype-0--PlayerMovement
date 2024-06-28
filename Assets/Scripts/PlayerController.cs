8c457d20ce3ec7d3e4e46594fc7dfa52b06aac95
Sack <rauldaxo@gmail.com>
2024-06-27 22:53:24 -0600

    Cleaner Code and Improvements
    We order things by using void functions in order to use them in Update method. Also, we commented some lines of code and made a backup of the project files.
    
    We created an emptyGameObject to bring together all the environment elements.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -17,6 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        ConstrainPlayerPosition();
+    }
+
+    void MovePlayer()
+    {
+        // We create and initialize all-direction movement variables for Input usage
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticallInput = Input.GetAxis("Vertical");
         playerRb.AddForce(Vector3.forward * speed * verticallInput);
@@ -27,7 +34,11 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         }
+    }
 
+    // Prevent the player from leaving from top or bottom of the screen
+    void ConstrainPlayerPosition()
+    {
         if (transform.position.z > zLimits)
         {
             playerRb.transform.position = new Vector3(transform.position.x, transform.position.y, zLimits);
@@ -37,7 +48,6 @@ public class PlayerController : MonoBehaviour
         {
             playerRb.transform.position = new Vector3(transform.position.x, transform.position.y, -zLimits);
         }
-
 
     }
 }
